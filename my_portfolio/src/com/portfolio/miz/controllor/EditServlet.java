86e6c1610de1dec82b06834e1762e2187ee85b5d
Sho_M <mizupon100@gmail.com>
2020-06-24 03:17:12 +0900

    各サーブレットでセッションをログアウトしていた場合にログイン画面に戻る要求を追加

--- my_portfolio/src/com/portfolio/miz/controllor/EditServlet.java
+++ my_portfolio/src/com/portfolio/miz/controllor/EditServlet.java
@@ -8,18 +8,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.portfolio.miz.model.DBConnector;
 import com.portfolio.miz.model.Items;
 import com.portfolio.miz.model.ItemsDao;
 import com.portfolio.miz.model.ItemsDaoImpl;
+import com.portfolio.miz.model.users.entity.Users;
 
 /**
  * Servlet implementation class EditServlet
  */
 @WebServlet("/EditServlet")
 public class EditServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
@@ -29,68 +31,76 @@ public class EditServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
 
         request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
+
+        HttpSession session = request.getSession();
+        Users userSession = (Users) session.getAttribute("login_info");
+        if (userSession == null) {
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+            return;
+        }
 
         String mode = request.getParameter("mode");
-	    String status = "処理に失敗しました";
+        String status = "処理に失敗しました";
 
-	    Items beans = new Items(request);
+        Items beans = new Items(request);
 
-	    try{
-	        DBConnector connector = new DBConnector();
-	        Connection conn = connector.connect();
-	        ItemsDao dao = new ItemsDaoImpl(conn);
+        try {
+            DBConnector connector = new DBConnector();
+            Connection conn = connector.connect();
+            ItemsDao dao = new ItemsDaoImpl(conn);
 
-	        switch (mode) {
+            switch (mode) {
 
-	        case "insert":
-	            boolean isInsertResult = dao.doInsert(beans);
-                if(isInsertResult) {
-                      status = "処理に成功しました";
-                    }
+            case "insert":
+                boolean isInsertResult = dao.doInsert(beans);
+                if (isInsertResult) {
+                    status = "処理に成功しました";
+                }
                 break;
 
-	        case "delete":
-	            boolean isDeleteResult = dao.doDelete(beans.getItemId());
-                if(isDeleteResult) {
+            case "delete":
+                boolean isDeleteResult = dao.doDelete(beans.getItemId());
+                if (isDeleteResult) {
                     status = "処理に成功しました";
-	                }
-	            break;
-
-	        case "setUpdate":
-	            request.setAttribute("beans", beans);
-	            request.getRequestDispatcher("/EditInput.jsp").forward(request, response);
-	            return;
-
-
-	        case "doUpdate":
-	            boolean isUpdateResult = dao.doUpdate(beans);
-            if(isUpdateResult) {
-                status = "処理に成功しました";
                 }
-            break;
-	        }
-	        // DBの切断
-	        connector.destory(conn);
-	    }catch(Exception e) {
-	          e.printStackTrace();
-	      }
-
-	    request.setAttribute("resultMessage", status);
-	    request.getRequestDispatcher("/result.jsp").forward(request, response);
+                break;
 
 	}
 
-	   /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+            case "doUpdate":
+                boolean isUpdateResult = dao.doUpdate(beans);
+                if (isUpdateResult) {
+                    status = "処理に成功しました";
+                }
+                break;
+            }
+            // DBの切断
+            connector.destory(conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        request.setAttribute("resultMessage", status);
+        request.getRequestDispatcher("/result.jsp").forward(request, response);
+
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+     *      response)
      */
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         // TODO Auto-generated method stub
         doPost(request, response);
     }
--- my_portfolio/src/com/portfolio/miz/controllor/ItemsSearcherServlet.java
+++ my_portfolio/src/com/portfolio/miz/controllor/ItemsSearcherServlet.java
@@ -10,18 +10,20 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.portfolio.miz.model.DBConnector;
 import com.portfolio.miz.model.Items;
 import com.portfolio.miz.model.ItemsDao;
 import com.portfolio.miz.model.ItemsDaoImpl;
+import com.portfolio.miz.model.users.entity.Users;
 
 /**
  * Servlet implementation class SearchServlet
  */
 @WebServlet("/ItemsSearcherServlet")
 public class ItemsSearcherServlet extends HttpServlet {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
     private List<Items> list = new ArrayList<Items>();
 
     /**
@@ -32,29 +34,38 @@ public class ItemsSearcherServlet extends HttpServlet {
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+     *      response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // TODO Auto-generated method stub
         request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-		    Items itemBeans = new Items(request);
-		    DBConnector connector = new DBConnector();
-            Connection conn = connector.connect();
-		    ItemsDao dao = new ItemsDaoImpl(conn);
-		    list = dao.fetch(itemBeans);
-		 // DBの切断
-            connector.destory(conn);
+        HttpSession session = request.getSession();
+        Users userSession = (Users) session.getAttribute("login_info");
+        if (userSession == null) {
+            request.getRequestDispatcher("/index.jsp").forward(request, response);
+            return;
+        }
 
-		    request.setAttribute("result", list);
+        Items itemBeans = new Items(request);
+        DBConnector connector = new DBConnector();
+        Connection conn = connector.connect();
+        ItemsDao dao = new ItemsDaoImpl(conn);
+        list = dao.fetch(itemBeans);
+        // DBの切断
+        connector.destory(conn);
+
+        request.setAttribute("result", list);
 
 //		    HttpSession session = request.getSession();
 //		    session.setAttribute("result", list);
 
-		    request.getRequestDispatcher("/SearchResult.jsp").forward(request, response);
+        request.getRequestDispatcher("/SearchResult.jsp").forward(request, response);
 
-		}
+    }
 
 }
